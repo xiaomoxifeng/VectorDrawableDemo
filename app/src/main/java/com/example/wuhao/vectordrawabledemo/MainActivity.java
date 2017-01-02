bcd4b7e740b880d2293a85693ca3dee9e152b55c
xiaomoxifeng <312770787@qq.com>
2017-01-02 23:21:22 +0800

    Initial commit

---
+++ app/src/main/java/com/example/wuhao/vectordrawabledemo/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.wuhao.vectordrawabledemo;
+
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void anim(View view) {
+        ImageView imageView = (ImageView) view;
+        Drawable drawable = imageView.getDrawable();
+        if (drawable instanceof Animatable) {
+            ((Animatable) drawable).start();
+        }
+    }
+}

225e1d324ddba87e6ac3f7a85333dee08a6d1936
xiaomoxifeng <312770787@qq.com>
2017-01-03 17:00:22 +0800

    first

--- app/src/main/java/com/example/wuhao/vectordrawabledemo/MainActivity.java
+++ app/src/main/java/com/example/wuhao/vectordrawabledemo/MainActivity.java
@@ -4,11 +4,14 @@ import android.graphics.drawable.Animatable;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
 import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    static {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
